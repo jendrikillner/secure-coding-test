425ec781b2f52f552a879aede2b44fb889076400
jendrikillner <jendrik.illner@gmail.com>
2025-01-07 18:33:33 -0400

    using ftell insteall

--- sample.c
+++ sample.c
@@ -4,6 +4,27 @@
  
 #define BUFSIZE 256
     
+long int getSizeOfFile(char const * file_name) 
+{ 
+    FILE* fp = fopen(file_name, "r"); 
+  
+    // ensure the file exists 
+    if (fp == NULL) { 
+        return -1; 
+    } 
+  
+    // seek to the end of the file
+    // so that we can query the size of the file
+    fseek(fp, 0L, SEEK_END);
+  
+    long int res = ftell(fp); 
+  
+    // closing the file 
+    fclose(fp); 
+  
+    return res; 
+} 
+
 // This program prints the size of a specified file in bytes
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
@@ -14,10 +35,12 @@ int main(int argc, char** argv) {
 
     char cmd[BUFSIZE];
 
-    int printedCharacters = snprintf(cmd, BUFSIZE, "wc -c < %s", argv[1]);
+    long int fileSize = getSizeOfFile(argv[1]);
 
-    if (printedCharacters>=0 && printedCharacters<BUFSIZE)
-    {
-        system(cmd);
+    if(fileSize != -1) {
+        fprintf(stdout, "File size: %li.\n", fileSize);
+        return
+    } else {
+        fprintf(stderr, "Failed to open file %s\n", file_name);
     }
 }
